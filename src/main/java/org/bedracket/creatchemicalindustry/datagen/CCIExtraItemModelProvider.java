854b4ae2f1fe703c51d3dea50091a9c74652e0ff
1798643961 <1798643961@qq.com>
2022-11-26 13:10:50 +0800

    修复流体材质

---
+++ src/main/java/org/bedracket/creatchemicalindustry/datagen/CCIExtraItemModelProvider.java
@@ -0,0 +1,17 @@
+package org.bedracket.creatchemicalindustry.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class CCIExtraItemModelProvider extends ItemModelProvider {
+
+    public CCIExtraItemModelProvider(DataGenerator generator, String modid, ExistingFileHelper existingFileHelper) {
+        super(generator, modid, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+
+    }
+}
--- src/main/java/org/bedracket/creatchemicalindustry/datagen/CCILanguageProvider.java
+++ src/main/java/org/bedracket/creatchemicalindustry/datagen/CCILanguageProvider.java
@@ -3,6 +3,7 @@ package org.bedracket.creatchemicalindustry.datagen;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
 import org.bedracket.creatchemicalindustry.CreateChemicalIndustry;
+import org.bedracket.creatchemicalindustry.init.ModFluids;
 import org.bedracket.creatchemicalindustry.init.ModItems;
 
 public class CCILanguageProvider extends LanguageProvider {
@@ -19,6 +20,8 @@ public class CCILanguageProvider extends LanguageProvider {
         add(ModItems.CALCIUM_CARBONATE_DUST.get(), "Calcium Carbonate Dust");
         add(ModItems.IMPURITY.get(), "Impurity");
         add(ModItems.CALCIUM_OXIDE_DUST.get(), "Calcium Oxide Dust");
+        add(ModFluids.ETHANOL.getBlock(), "Ethanol Fluid");
+        add(ModFluids.ETHANOL.getBucket(), "Ethanol Fluid Bucket");
         add("itemGroup.create_chemical_industry.material", "Material");
     }
 }
